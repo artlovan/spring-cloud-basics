165953b88f84af417d097582490ce396f1c6df3e
javadriven <macbookpro@pro.local>
2017-07-13 19:21:43 -0400

    added routing filter

--- hello-service/src/main/java/com/example/helloservice/HelloServiceApplication.java
+++ hello-service/src/main/java/com/example/helloservice/HelloServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,7 +17,7 @@ public class HelloServiceApplication {
     }
 
     @GetMapping
-    public String hello() {
-        return "Hello!";
+    public String hello(@RequestHeader("x-location") String location) {
+        return "Hello from " + location + "!";
     }
 }
