5b33076d67cc1b5206d6c606780679d9d248ae3a
javadriven <macbookpro@pro.local>
2017-07-22 07:25:48 -0400

    loadbalanced with @LoadBalanced

---
+++ ribbon-time-service/src/main/java/com/example/ribbontimeservice/RibbonTimeServiceApplication.java
@@ -0,0 +1,29 @@
+package com.example.ribbontimeservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class RibbonTimeServiceApplication {
+
+    @Value("${server.port}")
+    private int port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeServiceApplication.class, args);
+	}
+
+	@GetMapping
+    public String getTime() {
+        return "Current time is: " + new Date()
+                + " (answered by service " + port + ")";
+    }
+}
